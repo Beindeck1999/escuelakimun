061822f499e73cf2773837470b5fd809a6cf0e39
Beindeck1999 <javiera_luengo@escuelakimun.cl>
2025-11-20 11:45:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-// Aquí puedes agregar funciones más adelante (menú móvil, sliders, etc.)
-console.log("Sitio de Escuela Kimün cargado correctamente.");
+// Menú móvil
+const navToggle = document.querySelector('.nav-toggle');
+const mainNav = document.querySelector('.main-nav');
 
+if (navToggle && mainNav) {
+  navToggle.addEventListener('click', () => {
+    mainNav.classList.toggle('nav-open');
+  });
+}
+
+console.log("Sitio de Escuela Kimün cargado correctamente.");
